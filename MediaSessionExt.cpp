447e7a03fb02acf8cf94659b17e8f1e9fcfb556d
afranc083 <antonyxavier_francis@comcast.com>
2026-08-18 13:55:12 +0530

    Removed the unused function

--- MediaSessionExt.cpp
+++ MediaSessionExt.cpp
@@ -64,59 +64,6 @@ std::string convertToBase64(const std::vector<uint8_t>& data) {
     return out;
 }
 
-MediaKeySession::MediaKeySession(const uint8_t drmHeader[], uint32_t drmHeaderLength, DRM_APP_CONTEXT * poAppContext, bool initiateChallengeGeneration /* = false */)
-   : m_pbRevocationBuffer(nullptr)
-   , m_eKeyState(KEY_CLOSED)
-   , m_pbChallenge(nullptr)
-   , m_cbChallenge(0)
-   , m_pchSilentURL(nullptr)
-   , m_piCallback(nullptr)
-   , mSessionId(0)
-   , mInitiateChallengeGeneration(initiateChallengeGeneration)
-   , m_cHeaderKIDs(0)
-   , m_pdstrHeaderKIDs( nullptr )
-   , m_eHeaderVersion( DRM_HEADER_VERSION_UNKNOWN )
-   , m_oBatchID( DRM_ID_EMPTY )
-   , m_currentDecryptContext( nullptr )
-#ifdef USE_SVP
-   , m_pSVPContext(nullptr)
-   , m_rpcID(0)
-#endif
-   , m_fCommit(false)
-   , m_poAppContext(poAppContext)
-   , m_decryptInited(false)
-   , m_bDRMInitializedLocally(false)
-{
-    ZEROMEM(m_rgchSessionID, SIZEOF(m_rgchSessionID));
-
-    PR_LOG(PR_LOG_DEBUG, "entry");
-
-#ifdef USE_SVP
-    gst_svp_ext_get_context(&m_pSVPContext, Client, m_rpcID);
-    m_stSecureBuffInfo.bCreateSecureMemRegion = true;
-    m_stSecureBuffInfo.SecureMemRegionSize = 512 * 1024;
-
-    if( 0 != svp_allocate_secure_buffers(m_pSVPContext, (void**)&m_stSecureBuffInfo, nullptr, nullptr, m_stSecureBuffInfo.SecureMemRegionSize))
-    {
-        m_stSecureBuffInfo.SecureMemRegionSize = 0;
-    }
-#endif
-
-    mDrmHeader.clear();
-    mDrmHeader.resize(drmHeaderLength);
-
-    if(drmHeaderLength) {
-        memcpy(&mDrmHeader[0], drmHeader, drmHeaderLength);
-        std::string base64Header = convertToBase64(mDrmHeader); 
-        PR_LOG(PR_LOG_TRACE, "DRM Header size[%zu] (String):[%s]",mDrmHeader.size(), base64Header.c_str());
-    } else {
-        PR_LOG(PR_LOG_DEBUG, "drmHeaderLength is zero");
-    }
-
-    m_eKeyState = KEY_INIT;
-    PR_LOG(PR_LOG_DEBUG, "exit");
-}
-
 uint32_t MediaKeySession::GetSessionIdExt() const
 {
     PR_LOG(PR_LOG_DEBUG, "Get mSessionId[%d]", mSessionId);
